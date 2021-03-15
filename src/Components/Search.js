71b8affab681766ab1704a3616eaaa16eda9820e
jerry <brownjer3@gmail.com>
2021-03-15 17:19:36 -0500

    fix spacing for search input

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import Button from 'react-bootstrap/Button'
 import FormControl from 'react-bootstrap/FormControl'
 import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
 import InputGroup from 'react-bootstrap/InputGroup'
 
 class Search extends Component {
     render() {
         return(
-            <div className='d-flex justify-content-center'>
+            <>
                 <Form inline>
-                    <FormControl type="text" placeholder="Search" className="" size="lg"/>
-                    <Button variant="outline-info" size="lg">Search</Button>
-                </Form>
-            </div>
+                    <Col>
+                        <FormControl type="text" placeholder="Find a drop" className="mr-3 w-50" size="lg" />
+                        <Button variant="outline-info" size="lg">Search</Button>
+                    </Col>
+                </Form>  
+            </>
         )
     }
 }
