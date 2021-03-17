c8f7135f13802009cf8be062898e343d9a1ef3ac
jerry <brownjer3@gmail.com>
2021-03-16 22:18:59 -0500

    random drop to side-by-side layout

--- src/Containers/RandomContainer.js
+++ src/Containers/RandomContainer.js
@@ -4,7 +4,9 @@ import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import ListGroupItem from 'react-bootstrap/ListGroupItem'
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
-// import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 
 class RandomContainer extends Component {
     state = {
@@ -12,26 +14,28 @@ class RandomContainer extends Component {
     }
     render() {
         return (
-            
-            <Card className="bg-transparent border-0" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={this.state.image} />
-                {/* <video width='auto' height='300' loop autoplay playsinline src='{this.state.image}'></video> */}
-
-                <Card.Body>
-                    <Card.Title>Drop Title</Card.Title>
-                    <Card.Text>Creator's info</Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem className="bg-transparent">Date + Time</ListGroupItem>
-                    <ListGroupItem className="bg-transparent">Platform</ListGroupItem>
-                    <ListGroupItem className="bg-transparent">Type</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
+            <Card className="bg-transparent border-0" >
+                <Row>
+                    <Col >
+                        <Image src={this.state.image} style={{ height: '18rem' }}/>
+                    </Col>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Drop Title</Card.Title>
+                            <Card.Text>Creator's Info</Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem className="bg-transparent">Date + Time</ListGroupItem>
+                            <ListGroupItem className="bg-transparent">Platform</ListGroupItem>
+                            <ListGroupItem className="bg-transparent">Type</ListGroupItem>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Col>
+                </Row>
             </Card>
-            
         )
     }
 }
