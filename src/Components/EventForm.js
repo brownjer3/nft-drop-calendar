c1a120ccceae3eb1f5cbfc68bc2fa15cede7b6d1
jerry <brownjer3@gmail.com>
2021-03-16 21:21:03 -0500

    create new event form

--- src/Components/EventForm.js
+++ src/Components/EventForm.js
@@ -3,16 +3,65 @@ import Button from 'react-bootstrap/Button'
 import FormControl from 'react-bootstrap/FormControl'
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
-import InputGroup from 'react-bootstrap/InputGroup'
+import Container from "react-bootstrap/Container"
 
 class EventForm extends Component {
     render() {
         return(
-            <div>
-                {/* <Form inline> */}
-                test
-                {/* </Form>   */}
-            </div>
+            <Container className='p-3 text-start'>
+                <h1 className='my-3'>Drop a Drop</h1>
+                <Form >
+                    <Form.Row>
+                        <Form.Group as={Col} >
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" placeholder="Drop title" />
+                        </Form.Group>
+
+                    </Form.Row>
+                    <Form.Group controlId="formGridAddress1">
+                        <Form.Label>Creator</Form.Label>
+                        <Form.Control placeholder="Creator name" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridAddress2">
+                        <Form.Label>Platform</Form.Label>
+                        <Form.Control placeholder="Where's it dropping?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridAddress1">
+                        <Form.Label>Asset link</Form.Label>
+                        <Form.Control type='url' placeholder="Url " />
+                    </Form.Group>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Date</Form.Label>
+                            <Form.Control type="datetime-local" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>Type</Form.Label>
+                            <Form.Control as="select" defaultValue="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>Tags</Form.Label>
+                            <Form.Control as="select" defaultValue="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                    </Form.Row>
+
+                    <Button variant="info" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
         )
     }
 }
