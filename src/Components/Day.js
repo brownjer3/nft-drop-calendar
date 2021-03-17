91d867dd97c10ee9ee11d38ef6d57d050ac53594
jerry <brownjer3@gmail.com>
2021-03-16 21:25:51 -0500

    remove all references to old events container

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import EventsContainer from "../Containers/EventsContainer";
-import EventsTable from '../Containers/EventsTable'
 
 
 class Day extends Component {
@@ -9,11 +8,7 @@ class Day extends Component {
         return(
             <div>
                 <h3 className="d-flex pl-2">{this.props.date}</h3>
-                {/* <p>LAYOUT OPTION A</p>
                 <EventsContainer />
-                <br/>
-                <p>LAYOUT OPTION B</p> */}
-                <EventsTable />
             </div>
         )
     }
