f56fa570d08d7447bbf03c56fc3ddaf8536c99ba
jerry <brownjer3@gmail.com>
2021-03-18 14:35:54 -0500

    import moment.js and use for drop dates and times

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import EventsContainer from "../Containers/EventsContainer";
 import {connect} from 'react-redux'
+import moment from 'moment'
 
 
 class Day extends Component {
@@ -12,7 +13,7 @@ class Day extends Component {
     render() {
         return(
             <div>
-                <h3 className="d-flex pl-2">{this.props.date}</h3>
+                <h3 className="d-flex pl-2">{moment(this.props.date).format('MMMM Do YYYY')}</h3>
                 <EventsContainer events={this.filterEvents()} />
             </div>
         )
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarPlus } from '@fortawesome/free-solid-svg-icons'
 import Image from 'react-bootstrap/Image';
+import moment from 'moment'
 
 
 class Event extends Component {
     render() {
         return(
             <tr >
-                <td>{this.props.event.drop_time} <br/><FontAwesomeIcon icon={faCalendarPlus} size='lg' /></td>
+                <td>{moment(this.props.event.drop_time).format('h:mm a')} <br/><FontAwesomeIcon icon={faCalendarPlus} size='lg' /></td>
                 <td><Image rounded src={this.props.event.asset_url} style={{ height: '6rem' }}/></td>
                 <td>{this.props.event.title}</td>
                 <td>{this.props.event.creator_name}</td>
