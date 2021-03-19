7525bfdc557e7b5dda43fa4aca8c0e69df4a8b46
jerry <brownjer3@gmail.com>
2021-03-18 22:25:24 -0500

    implement randomization of events shown on discover page

--- src/Containers/Discover.js
+++ src/Containers/Discover.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import RandomContainer from './RandomContainer'
+import {connect} from 'react-redux'
 
 class Discover extends Component {
+
+    randomEvent = () => {
+        return this.props.events[Math.floor(Math.random() * this.props.events.length)]
+    }
+    
     render() {
         return(
             <div>
                 <div className="App-header">Random Drop</div> 
                 <Jumbotron className="calendar py-2 d-flex justify-content-center">
-                    <RandomContainer />
+                    <RandomContainer event={this.randomEvent()}/>
                 </Jumbotron>
             </div>
         )
     }
 }
 
-export default Discover
+const mapStateToProps = (state) => {
+    return {
+        events: state.events
+    }
+}
+
+export default connect(mapStateToProps)(Discover)
--- src/Containers/RandomContainer.js
+++ src/Containers/RandomContainer.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-// import { Container } from "react-bootstrap";
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import ListGroupItem from 'react-bootstrap/ListGroupItem'
@@ -8,33 +7,35 @@ import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
 class RandomContainer extends Component {
-    state = {
-        image: "https://res.cloudinary.com/nifty-gateway/image/upload/q_auto:good,w_500/v1615502630/AMatthew/NateHillMarch15/nate-auction-icon_j7frwi.png"
-    }
+    // state = {
+    //     image: "https://res.cloudinary.com/nifty-gateway/image/upload/q_auto:good,w_500/v1615502630/AMatthew/NateHillMarch15/nate-auction-icon_j7frwi.png"
+    // }
+
     render() {
         return (
-            <Card className="bg-transparent border-0" >
                 <Row>
-                    <Col >
-                        <Image rounded src={this.state.image} style={{ height: '18rem' }}/>
+                    <Col md={6}>
+                        <Image rounded src={this.props.event.asset_url} style={{ height: '18rem' }}/>
                     </Col>
                     <Col>
-                        <Card.Body>
-                            <Card.Title>Drop Title</Card.Title>
-                            <Card.Text>Creator's Info</Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem className="bg-transparent">Date + Time</ListGroupItem>
-                            <ListGroupItem className="bg-transparent">Platform</ListGroupItem>
-                            <ListGroupItem className="bg-transparent">Type</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
-                        </Card.Body>
+                        <Card className="bg-transparent border-0" >
+                            <Card.Body>
+                                <Card.Title>{this.props.event.title}</Card.Title>
+                                <Card.Text>{this.props.event.creator_name}</Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem className="bg-transparent">{this.props.event.drop_date}</ListGroupItem>
+                                <ListGroupItem className="bg-transparent">{this.props.event.platform_name}</ListGroupItem>
+                                <ListGroupItem className="bg-transparent">Type (TBD)</ListGroupItem>
+                            </ListGroup>
+                            <Card.Body>
+                                <Card.Link href="#">Link to Drop</Card.Link>
+                                {/* <Card.Link href="#">Another Link</Card.Link> */}
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
-            </Card>
+            
         )
     }
 }
