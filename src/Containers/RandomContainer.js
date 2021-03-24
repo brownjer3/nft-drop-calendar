4e15e6b73e3ed88d4724a0a702705209cbf18f2e
jerry <brownjer3@gmail.com>
2021-03-23 17:29:01 -0700

    adjust format of discover page

--- src/Containers/RandomContainer.js
+++ src/Containers/RandomContainer.js
@@ -9,24 +9,23 @@ import Image from 'react-bootstrap/Image'
 class RandomContainer extends Component {
     render() {
         return (
-                <Row className="h-100">
-                    <Col md={6}>
-                        <Image rounded src={this.props.event.asset_url} style={{ height: '18rem' }}/>
+                <Row className="w-100 d-flex">
+                    <Col md={6} className="text-right">
+                        <Image fluid rounded src={this.props.event.asset_url} style={{ height: '18rem' }}/>
                     </Col>
-                    <Col md={6}>
+                    <Col md={6} className="text-left">
                         <Card className="bg-transparent border-0">
                             <Card.Body>
                                 <Card.Title>{this.props.event.title}</Card.Title>
                                 <Card.Text>{this.props.event.creator_name}</Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
-                                <ListGroupItem className="bg-transparent">Dropping: <br/>{this.props.event.drop_date} @ [TIME]</ListGroupItem>
+                                <ListGroupItem className="bg-transparent">Dropping at <br/>{this.props.event.drop_datetime}</ListGroupItem>
                                 <ListGroupItem className="bg-transparent">{this.props.event.platform_name}</ListGroupItem>
-                                <ListGroupItem className="bg-transparent">Type (TBD)</ListGroupItem>
+                                <ListGroupItem className="bg-transparent">{this.props.event.type}</ListGroupItem>
                             </ListGroup>
                             <Card.Body>
-                                <Card.Link href="#">Link to Drop</Card.Link>
-                                {/* <Card.Link href="#">Another Link</Card.Link> */}
+                                <Card.Link href={this.props.event.drop_link} target='_blank'>View Drop</Card.Link>
                             </Card.Body>
                         </Card>
                     </Col>
