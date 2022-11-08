79721f8a899b9effac4cb591b0bda7aa1975995c
Jerry Brown <jerrybrown@MacBook-Pro.lan>
2022-11-08 14:52:31 -0800

    testing github connection

--- src/Containers/TweetsContainer.js
+++ src/Containers/TweetsContainer.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import Container from 'react-bootstrap/Container'
-
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+import Container from "react-bootstrap/Container";
 
 class TweetsContainer extends Component {
-    render() {
-        return (
-            <Container className='w-50'>
-                <div>
-                    <TwitterTimelineEmbed 
-                        sourceType="list" 
-                        id="1372285702868758535" 
-                        // noHeader
-                        noFooter
-                        noBorders
-                        // noScrollbar
-                        transparent
-                        theme='dark'
-                        options={{height: 600}}
-                    />
-                </div>
-            </Container>
-        )
-    }
+	// test
+	render() {
+		return (
+			<Container className="w-50">
+				<div>
+					<TwitterTimelineEmbed
+						sourceType="list"
+						id="1372285702868758535"
+						// noHeader
+						noFooter
+						noBorders
+						// noScrollbar
+						transparent
+						theme="dark"
+						options={{ height: 600 }}
+					/>
+				</div>
+			</Container>
+		);
+	}
 }
 
-export default TweetsContainer
+export default TweetsContainer;
